f09a6a6fd096d6af4f947149d7aef95189162bc6
Erico Andre <ericoandresilva@gmail.com>
2024-11-15 15:48:45 -0300

    Update test_main.cpp

--- test/test_WiFi/test_main.cpp
+++ test/test_WiFi/test_main.cpp
@@ -1,16 +1,1 @@
-#include <Arduino.h>
-#include <unity.h>
-#include <ESP8266WiFi.h>
 
-const char* ssid = "intel";
-const char* passwd = "12345678";
-
-void connet(){
-  WiFi.begin(ssid, passwd);
-  Serial.println("Connecting....");
-  while (WiFi.status() != WL_CONNECTED){
-    delay(50);
-    Serial.print(".");
-  }
-  Serial.println(WiFi.localIP());
-}
